cfc4d16e7f9dc4c5f3e04e446881c1347194a53f
Anastasia <sacurasama@gmail.com>
2024-10-15 21:55:02 +0300

    Feat: wrote the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const arrNumber = [...String(n)];
+   arrNumber.reverse();
+    let newNumber = " ";
+    for(let i = 0; i < arrNumber.length; i++ ){
+        newNumber += arrNumber[i];
+    }
+    return  Number(newNumber);
 }
